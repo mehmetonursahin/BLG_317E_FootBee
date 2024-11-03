53d0e9ad3f146a72b5ba80528e32ede8d5da536f
itu-itis22-sahinme21 <sahinme21@itu.edu.tr>
2024-11-03 20:06:27 +0300

    CREATE commands of players and player_valuations are added

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,4 +1,5 @@
 CREATE TYPE game_event_type AS ENUM ('Substitutions', 'Goals');
+CREATE TYPE foot_used AS ENUM ('NotIndicated', 'Left', 'Right', 'Both')
 
 CREATE TABLE "game_events"(
     "game_id" INTEGER NOT NULL,
@@ -39,3 +40,39 @@ ALTER TABLE
     "games" ADD PRIMARY KEY("game_id");
 ALTER TABLE
     "games" ADD CONSTRAINT "games_game_id_foreign" FOREIGN KEY("game_id") REFERENCES "game_events"("game_id");
+
+CREATE TABLE "players" (
+    "player_id" INTEGER PRIMARY KEY,
+    "first_name" VARCHAR(50) NOT NULL,
+    "last_name" VARCHAR(50) NOT NULL,
+    "last_season" INTEGER,
+    "current_club_id" INTEGER,
+    "player_code" VARCHAR(100) NOT NULL,
+    "country_of_birth" VARCHAR(50) NOT NULL,
+    "city_of_birth" VARCHAR(50) NOT NULL,
+    "country_of_citizenship" VARCHAR(50) NOT NULL,
+    "date_of_birth" DATE NOT NULL,
+    "sub_position" VARCHAR(50) NOT NULL,
+    "position" VARCHAR(50) NOT NULL,
+    "foot" foot_used NOT NULL,
+    "height_in_cm" INTEGER,
+    "contract_expiration_date" DATE,
+    "agent_name" VARCHAR(100),
+    "image_url" VARCHAR(255) NOT NULL,
+    "url" VARCHAR(255) NOT NULL
+
+    FOREIGN KEY ("current_club_id") REFERENCES "clubs"("club_id")
+    ON DELETE SET NULL
+    ON UPDATE CASCADE
+);
+
+CREATE TABLE "player_valuations" (
+    "player_id" INTEGER,
+    "datetime" DATETIME,
+    "market_value_in_eur" INTEGER NOT NULL,
+    "highest_market_value_in_eur" INTEGER NOT NULL,
+    "current_club_id" INTEGER NOT NULL,
+
+    PRIMARY KEY ("player_id", "datetime"),
+    FOREIGN KEY ("player_id") REFERENCES "players"("player_id")
+);
